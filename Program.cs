773c3e0fcad4b03407b8d07c75d813a74d052e2f
Angel <angelusualle@gmail.com>
2018-10-27 18:02:49 -0400

    Simple Service Created

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
-
-namespace Skraper3
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿using System.IO;  
+using System.Threading.Tasks;  
+using Microsoft.Extensions.Configuration;  
+using Microsoft.Extensions.DependencyInjection;  
+using Microsoft.Extensions.Hosting;  
+using Microsoft.Extensions.Logging;  
+using Serilog;  
+  
+namespace Skraper3 
+{  
+    class Program  
+    {  
+        static async Task Main(string[] args)  
+        {  
+            IHost host = new HostBuilder()  
+                 .ConfigureHostConfiguration(configHost =>  
+                 {  
+                     configHost.SetBasePath(Directory.GetCurrentDirectory());  
+                     configHost.AddEnvironmentVariables(prefix: "ASPNETCORE_");  
+                     configHost.AddCommandLine(args);  
+                 })  
+                 .ConfigureAppConfiguration((hostContext, configApp) =>  
+                 {  
+                     configApp.SetBasePath(Directory.GetCurrentDirectory());  
+                     configApp.AddEnvironmentVariables(prefix: "ASPNETCORE_");  
+                     configApp.AddJsonFile($"appsettings.json", true);  
+                     configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true);  
+                     configApp.AddCommandLine(args);  
+                 })  
+                .ConfigureServices((hostContext, services) =>  
+                {  
+                    services.AddLogging();  
+                    services.AddHostedService<Skraper3Service>();  
+                })  
+                .ConfigureLogging((hostContext, configLogging) =>  
+                {  
+                    configLogging.AddSerilog(new LoggerConfiguration()  
+                              .ReadFrom.Configuration(hostContext.Configuration)  
+                              .CreateLogger());  
+                    configLogging.AddConsole();  
+                    configLogging.AddDebug();  
+                })  
+                .Build();  
+  
+            await host.RunAsync();  
+        }  
+  
+  
+    }  
+} 
